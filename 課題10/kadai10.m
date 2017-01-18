376f6ce7b47682af9ced90edd497c0911d3f8209
Shoki Kobayashi <14ec052@ms.dendai.ac.jp>
2017-01-18 15:41:50 +0900

    編集

--- 課題10/kadai10.m
+++ 課題10/kadai10.m
@@ -3,7 +3,7 @@
 % 各自，Lenna以外の画像を用いよ．
 % 例
 
-ORG = imread('Lenna.jpg'); % 原画像の入力
+ORG = imread('https://www.pakutaso.com/shared/img/thumb/NISSIHIGEPENGIN_TP_V.jpg'); % 原画像の入力
 ORG = rgb2gray(ORG); %カラーからグレイへの変換
 imagesc(ORG); colormap('gray'); colorbar;% 画像表示
 pause; % 一時停止
